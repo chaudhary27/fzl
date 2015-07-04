cb3bdf7042051151b9597d982e2ffd84a5d6f363
Faisal <chaudhary.fsl@gmail.com>
2015-07-04 05:40:30 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fzl::Application.config.secret_key_base = '2d91cfcbbc083ad4c16c65f2d046dc5ef6aceb7b8922a66f291e5f727119a3dc7babd95d27bd2a9956623c4c69ca7fd057507161a4b1f5b831879b0a44729075'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fzl::Application.config.session_store :cookie_store, key: '_fzl_session'
